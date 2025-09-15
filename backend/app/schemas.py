02146fe017ea112eae6d167f20802bd61d41208d
HemaSai09 <102684800+HemaSai09@users.noreply.github.com>
2025-09-15 15:22:30 +0530

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,22 @@
+from pydantic import BaseModel
+
+class CourseBase(BaseModel):
+    course_name: str
+    department: str
+    level: str
+    delivery_mode: str
+    credits: int
+    duration_weeks: int
+    rating: float
+    tuition_fee_inr: int
+    year_offered: int
+
+class CourseCreate(CourseBase):
+    pass
+
+class CourseOut(CourseBase):
+    course_id: int
+
+    class Config:
+        orm_mode = True
 
